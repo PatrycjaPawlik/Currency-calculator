0818cdda4833a61b6e04a30f98fcc85b349a4137
Patrycja Pawlik <patrycjapawlik21@gmail.com>
2022-05-23 12:40:13 +0200

    added onFormSubmit

--- js/script.js
+++ js/script.js
@@ -10,26 +10,30 @@ const calculateResult = (amount, currency) => {
     }
 };
 
+const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const amountElement = document.querySelector(".js-amount");
+    const resultElement = document.querySelector(".js-result");
+    const currencyElement = document.querySelector(".js-currency");
+
+    const amount = +amountElement.value;
+    const currency = currencyElement.value;
+
+    const result = calculateResult(amount, currency);
+
+    resultElement.innerText = `${amount} PLN = ${result.toFixed(2)} ${currency} `;
+};
+
 const init = () => {
     const formElement = document.querySelector(".js-form");
 
-    formElement.addEventListener("submit", (event) => {
-        event.preventDefault();
-
-        const amountElement = document.querySelector(".js-amount");
-        const resultElement = document.querySelector(".js-result");
-        const currencyElement = document.querySelector(".js-currency");
-
-        const amount = +amountElement.value;
-        const currency = currencyElement.value;
-
-        const result = calculateResult(amount, currency);
-
-        resultElement.innerText = `${amount} PLN = ${result.toFixed(2)} ${currency} `;
-    });
+    formElement.addEventListener("submit", onFormSubmit);
 };
 
-init ();
+init();
+
+
 
 
 
